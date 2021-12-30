1beda2c01842963ec8d59fd9c6934583eb9aa010
Jane <jane.tecnicavocal@gmail.com>
2021-12-30 15:42:58 -0300

    Conceito de herança de classes

--- Programando com Orientação a Objeto com C#/ExemploPOO/Program.cs
+++ Programando com Orientação a Objeto com C#/ExemploPOO/Program.cs
@@ -1,22 +1,33 @@
 ﻿using System;
-using ExemploPOO;
 using ExemploPOO.Models;
 
 namespace ExemploPOO
 {
     class Program
     {
-        static void Main(string[]args)
+        static void Main(string[] args)
         {
-            //Valores válidos
-            Retangulo r = new Retangulo();
-            r.DefinirMedidas(30, 30);
-            System.Console.WriteLine($"Área: {r.ObterArea()}");
 
-            //Valores inválidos
-            Retangulo r2 = new Retangulo();
-            r2.DefinirMedidas(0, 0);
-            System.Console.WriteLine($"Área: {r2.ObterArea()}");
+            Professor p1 = new Professor();
+            p1.Salario = 2000;
+            p1.Documento = "123456";
+            p1.Nome = "Bob";
+            p1.Idade = 20;
+            p1.Apresentar();
+            Console.WriteLine();
+
+
+        
+
+            // //Valores válidos
+            // Retangulo r = new Retangulo();
+            // r.DefinirMedidas(30, 30);
+            // System.Console.WriteLine($"Área: {r.ObterArea()}");
+
+            // //Valores inválidos
+            // Retangulo r2 = new Retangulo();
+            // r2.DefinirMedidas(0, 0);
+            // System.Console.WriteLine($"Área: {r2.ObterArea()}");
 
             //Pessoa p1 = new Pessoa ();
 
