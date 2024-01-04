2ef0fea7fc6d92c445b467cb5cb40581c4f74441
Jane Nogueira <85239045+JaneNogueira@users.noreply.github.com>
2024-01-04 20:43:43 -0300

    organização de repositório

---
+++ FormaçãoDotNet/desafio-poo-celular/Program.cs
@@ -0,0 +1,16 @@
+﻿
+﻿using DesafioPOO.Models;
+
+Console.WriteLine("Nokia:");
+Smartphone nokia = new Nokia(numero: "622102102", modelo: "500", imei: "121a6596252362", memoria: 128);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("IPhone:");
+Smartphone iphone = new Iphone(numero: "1541542323", modelo: "6 Plus", imei: "451221220d1211", memoria: 264);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Facebook");
---
+++ FormaçãoDotNet/desafio-projeto-instagram-banco/PROJ.jogorpg/JogoRPG/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using JogoRPG.src.Entities;
+
+namespace dotnet_poo
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Knight arus = new Knight("Arus", 23, "Knight");
+            Wizard wizard = new Wizard("Jennica", 25, "White Wizard");
+       
+            Console.WriteLine(wizard.Attack(1));
+            Console.WriteLine(wizard.Attack(8));
+            
+        }
+    }
+}
+
