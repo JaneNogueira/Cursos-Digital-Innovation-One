13d3e2712b8e62353e307d662cf8371878966949
Jane Nogueira <85239045+JaneNogueira@users.noreply.github.com>
2024-01-04 20:40:52 -0300

    organização repositório

---
+++ FormaçãoDotNet/Controlando o Fluxo de Execução/Aula1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int valor1, valor2, soma;      
+    
+        Console.WriteLine("Digite seu primeiro valor: ");
+        valor1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Digite o segundo valor: ");
+        valor2 = int.Parse(Console.ReadLine());
+        soma = valor1 + valor2;
+        Console.WriteLine($"A soma de seus valores é: {soma}");
+        
+    }
+}
+
