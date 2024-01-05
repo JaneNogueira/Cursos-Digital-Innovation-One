e445c6a1b4ef8a992d5a3e7774e444a80dbed17c
Jane Nogueira <85239045+JaneNogueira@users.noreply.github.com>
2024-01-04 21:11:20 -0300

    Desafios de Código

---
+++ Desafios de Código/README.md
@@ -0,0 +1,2 @@
+# DesafiosDeCodigo
+## Nesta pasta estão os desafios de código C# proposto no curso "Programando em C#" da DIO - Digital Innovation One e conta com 6 desafios resolvidos em C#
---
+++ Desafios de Código/Xenlongao/Xenlongao.cs
@@ -0,0 +1,17 @@
+﻿namespace DesafioCodigoDio
+{
+    class Xenlongao
+    {
+        static void Main(string[] args)
+        {
+            int Numero = Convert.ToInt32(Console.ReadLine());
+            while(Numero -- > 0)
+            {
+                int valor = Convert.ToInt32(Console.ReadLine());
+                int novoValor = (int)Math.Sqrt(valor);     
+                int resposta = valor - novoValor;
+                Console.WriteLine(resposta);
+            }
+        }
+    }
+}
